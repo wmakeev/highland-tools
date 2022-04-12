fdc7c912a854d6706aa01e3e6e80ec4ee8f7cc4f
Vitaliy V. Makeev <w.makeev@gmail.com>
2022-04-12 20:29:07 +0500

    feat(append): add append with fixed typings

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,10 @@
 export function compact<T>(source: Highland.Stream<T>) {
   return source.compact() as unknown as Highland.Stream<NonNullable<T>>
 }
+
+export function append<U>(value: U) {
+  return <T>(source: Highland.Stream<T>): Highland.Stream<T | U> => {
+    // @ts-expect-error fix type union
+    return source.append(value)
+  }
+}
--- test/types.test.ts
+++ test/types.test.ts
@@ -5,7 +5,7 @@
 import test from 'tape'
 import _H from 'highland'
 
-import { compact } from '../src'
+import { compact, append } from '../src'
 
 test('types/compact', t => {
   const values = [1, null, 1, 2, null, 1, 3]
@@ -25,4 +25,38 @@ test('types/compact', t => {
     t.deepEqual(arr, [1, 1, 2, 1, 3])
     t.end()
   })
+})
+
+test('types/append', t => {
+  t.plan(4)
+
+  const values = [1, 2, 3]
+
+  _H(values)
+    .append(4)
+    .toArray(nums => {
+      t.deepEqual(nums, [1, 2, 3, 4])
+    })
+
+  _H(values)
+    // @ts-expect-error should get standard typings error
+    .append('four')
+    .toArray(nums => {
+      t.deepEqual(nums, [1, 2, 3, 'four'])
+    })
+
+  _H(values)
+    .through(append(4))
+    .toArray(nums => {
+      t.deepEqual(nums, [1, 2, 3, 4])
+    })
+
+  _H(values)
+    .through(append('four'))
+    .toArray(nums => {
+      const result: Exclude<typeof nums[number], number> = 'str'
+      result
+
+      t.deepEqual(nums, [1, 2, 3, 'four'])
+    })
 })
